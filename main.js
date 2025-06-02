ae2833f05a7cc16dbdd80de87271e27b9700e699
zesciati <zakyabiyyu2003@gmail.com>
2025-06-02 12:20:55 +0800

    coba ulang

--- main.js
+++ main.js
@@ -1,29 +1,20 @@
-// NOTE - Memanggil class dengan query selector
 const storageInput = document.querySelector('.storage');
 const text = document.querySelector('.text');
 const button = document.querySelector('.button');
-const storedInput = localStorage.getItem('textinput');
-
+const saveInput = localStorage.getItem('textinput')
 
 if (storageInput) {
-  text.textContent = storedInput
+  text.textContent = saveInput
 }
 
-
-// NOTE - addEventListener, Setiap kata yang diketik di input maka text akan mengikuti
-/*
-storageInput  --memicu--> event input --> kirim objek event (letter) ke callback
-callback menerima parameter letter --> kita pakai letter.target (storageInput) dan letter.target.value (isi input)
-*/
 storageInput.addEventListener('input', letter => {
+  console.log(letter);
   text.textContent = letter.target.value
-})
+});
 
-// NOTE - Kode ini akan menyimpan konten text ke local storage 
-const saveToLocalStorage = () => {
-  localStorage.setItem('textinput', text.textContent);
+const storedInput = () => {
+  //                      key     :  value  
+  localStorage.setItem('textinput', text.textContent)
+};
 
-}
-
-//NOTE - Button save untuk konten agar tersave 
-button.addEventListener('click', saveToLocalStorage)
+button.addEventListener('click', storedInput)
